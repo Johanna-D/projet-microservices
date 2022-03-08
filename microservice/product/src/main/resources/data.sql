4a862699683bf0b74fa6ce851faec44efddf8361
Lindoryel <lindoriel02@gmail.com>
2022-03-08 09:34:21 +0100

    Modif pour data sql, version de base fonctionnelle

--- microservice/product/src/main/resources/data.sql
+++ microservice/product/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO PRODUCT(id,name, description, illustration, price)
+INSERT INTO product(id,name, description, illustration, price)
 values (0, 'Produit0', 'description produit 0','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
        (1, 'Produit1', 'description produit 1','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
        (2, 'Produit2', 'description produit 2','https://images.squarespace-cdn.com/content/v1/576c0682f5e231d654cf661c/1528449391201-4OIJK4NQYFQTD8Y102W2/shopping-cart.png',9.9),
